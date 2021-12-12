e1a865a6224ba557b1ee585b5fec8c6d82d9fae4
waffles <76921051+waffles08@users.noreply.github.com>
2021-12-11 19:03:08 -0500

    Create perms_handler.js
    THIS IS NOT A COMMAND!
    this is simply just a permisson handler for the "ADMIN" permssions.
    This will simply allow you to use a certain command if you put this code at the top

---
+++ perms_handler.js
@@ -0,0 +1,8 @@
+/* THIS IS NOT A COMMAND!
+this is simply just a permisson handler for the "ADMIN" permssions.
+This will simply allow you to use a certain command if you put this code at the top */
+
+if(message.member.permissions.has("ADMINISTRATOR")){ /*code for here
+brrrrr*/    
+  
+}
