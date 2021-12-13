4c2e24d67e04a1e2b2a6d3669976a00aaa92c43a
waffles <76921051+waffles08@users.noreply.github.com>
2021-12-13 12:01:36 -0500

    Create status.js
    Shows the bots status as streaming and a yt or twitch channel of your choice.
    Free self promo

---
+++ status.js
@@ -0,0 +1,13 @@
+//THIS CODE MUST GO IN YOUR MAIN FILE ON YOUR BOT
+
+
+ 
+client.once('ready', () => {
+    console.log('the bot is online');
+    client.user.setActivity("with ur mum", {
+  type: "STREAMING",
+  url: "https://www.twitch.tv/waffles_gaming08"
+}); 
+  
+  
+  //enjoy
